1c1568820b239d0756b50db14d6b4f6c05f03d58
microjumper <microjmpr@gmail.com>
2022-04-05 14:24:54 +0200

    Starter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
     pathMatch: 'full'
   },
-  {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
 ];
 
 @NgModule({
